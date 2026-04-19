549087ca89eff4a88ceef93a2e0959a4235db065
sam cocquyt <samcocquyt04@gmail.com>
2026-04-19 09:31:53 -0400

    P2 - created the alien, displayed on screen, no movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
+from alien import Alien
 
 class AlienInvasion:
 
@@ -30,6 +31,7 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.2)
 
         self.ship = Ship(self, Arsenal(self))
+        self.alien = Alien(self, 10, 10)
     
     def run_game(self):
         #game loop
@@ -37,12 +39,14 @@ class AlienInvasion:
         while self.running:
             self._check_events()
             self.ship.update()
+            self.alien.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
+        self.alien.draw_alien()
         pygame.display.flip()
 
     def _check_events(self):
--- settings.py
+++ settings.py
@@ -20,3 +20,8 @@ class Settings:
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
+
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 5
